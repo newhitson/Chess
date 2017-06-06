228707fab2bb3720c0a6dcedd1f5dc6d69d46825
App Academy Student <student@appacademy.io>
2017-06-05 17:52:09 -0700

    build board and display

---
+++ board.rb
@@ -0,0 +1,59 @@
+require_relative 'piece.rb'
+
+class Board
+  attr_reader :board, :grid
+  def initialize
+    @grid = Board.piece_setup
+  end
+
+  def self.piece_setup
+    make_board = Array.new(8){Array.new(8){nil}}
+    make_board.each_with_index do |row, idx1|
+      row.each_with_index do |tile, idx2|
+        if idx1 > 1 && idx1 < 6
+          make_board[idx1][idx2] = NullPiece.new
+        else
+          make_board[idx1][idx2] = Piece.new
+        end
+      end
+      make_board
+    end
+
+
+
+  end
+
+
+  def board_in_bound
+
+  end
+
+  def move_piece(start_pos, end_pos)
+    raise OffBoardError.new "Location not on board" unless end_pos[0].between?(0,7) && end_pos[0].between?(0,7)
+    val_at_start = @grid[start_pos[0]][start_pos[1]]
+    val_at_end = @grid[end_pos[0]][end_pos[1]]
+
+
+    raise NotAPieceError.new "Thats not a piece!!!!" if val_at_start.is_a?(NullPiece)
+    @grid[end_pos[0]][end_pos[1]] = val_at_start
+    @grid[start_pos[0]][start_pos[1]] = val_at_end
+
+  end
+
+  # def [](pos)
+  #   x,y = pos
+  #   @grid[x][y]
+  # end
+  #
+  # def []=(pos, value)
+  #   x,y = pos
+  #   @grid[x][y] = value
+  # end
+
+
+end
+
+class OffBoardError < StandardError
+end
+class NotAPieceError < StandardError
+end
---
+++ display.rb
@@ -0,0 +1,30 @@
+require 'colorize'
+require_relative 'cursor.rb'
+require_relative 'board.rb'
+
+class Display
+  attr_reader :board
+  def initialize(board)
+    @board = board
+    @cursor = Cursor.new([0,0], board)
+  end
+
+  def move(new_pos)
+  end
+
+  def render
+    @board.grid.each do |row|
+      puts
+      row.each do |tile|
+        if tile.is_a?(NullPiece)
+          print " N "
+        else
+          print " P "
+        end
+      end
+
+    end
+     puts"" #do not delete this seeming pointless line the last line wont render
+  end
+
+end
